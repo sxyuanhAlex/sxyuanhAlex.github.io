bb0fd134f81cc85c58498d4cd40a5f0f865120c3
sxyuanhAlex <alexcoobi@gmail.com>
2019-09-28 01:29:57 +0800

    v4

---
+++ themes/next/source/dist/music.js
@@ -0,0 +1,52 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+	
+      {
+        name: 'Highway To Hell',
+        artist: 'AC/DC',
+        url: 'https://win-web-rh01-sycdn.kuwo.cn/bda79161008e679547c8d784ec415099/5d8e43db/resource/n2/26/3/3691742011.mp3',
+        cover: 'http://img4.kuwo.cn/star/albumcover/300/61/42/2668898083.jpg',
+      },
+      
+      {
+        name: 'Viva La Vida',
+        artist: 'Coldplay',
+        url: 'https://rp01-sycdn.kuwo.cn/3029abd00c169165c80652c91fa389fa/5d8e38ae/resource/n2/26/66/3590184758.mp3',
+        cover: 'http://img2.sycdn.kuwo.cn/star/albumcover/300/90/80/2251451924.jpg',
+      },
+      {
+        name: '常夜灯',
+        artist: '玉置浩二',
+        url: 'https://win-web-ra01-sycdn.kuwo.cn/c3336f679a957936e4704b8c36311687/5d8e3959/resource/n1/128/60/9/2438811764.mp3',
+        cover: 'http://img3.kuwo.cn/star/albumcover/300/40/13/1229638794.jpg',
+      },
+      {
+        name: 'In This River',
+        artist: 'Black Label Society',
+        url: 'https://win-web-rg01-sycdn.kuwo.cn/dfb5ea87599ab569b47c901bab065f60/5d8e3676/resource/n1/66/41/1809995527.mp3',
+        cover: 'http://img1.kuwo.cn/star/albumcover/300/82/19/2916131621.jpg',
+      },
+      {
+        name: '给我一点温度',
+        artist: '梁博',
+        url: 'https://win-web-rh01-sycdn.kuwo.cn/4aeff13d04fdfe1c768e744110d451d8/5d8e3ef2/resource/n3/23/32/2054865844.mp3',
+        cover: 'http://img1.kuwo.cn/star/albumcover/300/1/78/903443910.jpg',
+      },
+      {
+        name: '日落大道',
+        artist: '梁博',
+        url: 'https://win-web-rg01-sycdn.kuwo.cn/9d835662d91c5233a83f21d37b334a1b/5d8e3f54/resource/n2/64/42/2187849568.mp3',
+        cover: 'http://img4.kuwo.cn/star/albumcover/120/86/67/1941541207.jpg',
+      },
+      {
+        name: '融化',
+        artist: '梁博',
+        url: 'https://win-web-rg01-sycdn.kuwo.cn/3e57e876391030429e6883dfcdcef995/5d8e3fb4/resource/n3/12/78/2938450662.mp3',
+        cover: 'http://img4.kuwo.cn/star/albumcover/120/86/67/1941541207.jpg',
+      }
+    ]
+    
+});
