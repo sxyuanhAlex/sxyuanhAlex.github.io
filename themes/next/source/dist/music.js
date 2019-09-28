e5549aaa0c234bdccf6d9eeefe0891044cb05824
sxyuanhAlex <alexcoobi@gmail.com>
2019-09-29 01:35:02 +0800

    v5

--- themes/next/source/dist/music.js
+++ themes/next/source/dist/music.js
@@ -3,14 +3,6 @@ const ap = new APlayer({
     fixed: true,
     autoplay: false,
     audio: [
-	
-      {
-        name: 'Highway To Hell',
-        artist: 'AC/DC',
-        url: 'https://win-web-rh01-sycdn.kuwo.cn/bda79161008e679547c8d784ec415099/5d8e43db/resource/n2/26/3/3691742011.mp3',
-        cover: 'http://img4.kuwo.cn/star/albumcover/300/61/42/2668898083.jpg',
-      },
-      
       {
         name: 'Viva La Vida',
         artist: 'Coldplay',
@@ -28,18 +20,6 @@ const ap = new APlayer({
         artist: 'Black Label Society',
         url: 'https://win-web-rg01-sycdn.kuwo.cn/dfb5ea87599ab569b47c901bab065f60/5d8e3676/resource/n1/66/41/1809995527.mp3',
         cover: 'http://img1.kuwo.cn/star/albumcover/300/82/19/2916131621.jpg',
-      },
-      {
-        name: '给我一点温度',
-        artist: '梁博',
-        url: 'https://win-web-rh01-sycdn.kuwo.cn/4aeff13d04fdfe1c768e744110d451d8/5d8e3ef2/resource/n3/23/32/2054865844.mp3',
-        cover: 'http://img1.kuwo.cn/star/albumcover/300/1/78/903443910.jpg',
-      },
-      {
-        name: '日落大道',
-        artist: '梁博',
-        url: 'https://win-web-rg01-sycdn.kuwo.cn/9d835662d91c5233a83f21d37b334a1b/5d8e3f54/resource/n2/64/42/2187849568.mp3',
-        cover: 'http://img4.kuwo.cn/star/albumcover/120/86/67/1941541207.jpg',
       },
       {
         name: '融化',
